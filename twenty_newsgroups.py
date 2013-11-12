587f00e259f44befda7fd03ae386cbaadc70fb66
byron wallace <byron.wallace@gmail.com>
2013-11-11 20:15:15 -0500

    checking in twenty newsgroups code

--- twenty_newsgroups.py
+++ twenty_newsgroups.py
@@ -12,7 +12,6 @@ newsgroups_train = fetch_20newsgroups(subset="train", categories=cats)
 
 vectorizer = CountVectorizer(stop_words="english", max_features=5000)
 vectors = vectorizer.fit_transform(newsgroups_train.data)
-ndarrays = vectors_to_list(vectors)
 
 # this was the easiest way to interface with gensim... obviously
 # wasteful to go to disk (and back), but good for now.
@@ -20,17 +19,29 @@ with open("newsgroups.txt", 'w') as f:
     dump_svmlight_file(vectors, newsgroups_train.target, f)
 
 corp = SvmLightCorpus("newsgroups.txt")
-gLDA = gensim_LDA(corp, alpha=5, eta=20, num_topics=2)
+gLDA = gensim_LDA(corp, alpha=1, eta=2, num_topics=2)
 gLDA_lambda = gLDA.state.get_lambda()
-
 print "ok -- gensim model fit"
 
+#### yalda
 yalda_model = lda.CollapsedSampler(
-        alpha = 5., # number-of-topics-in-document concentration parameter
-        beta = 20., # number-of-words-in-topic concentration parameter
+        alpha = 1., # number-of-topics-in-document concentration parameter
+        beta = 2., # number-of-words-in-topic concentration parameter
         num_topics = 2,
-        num_vocab = 5000)
+        num_vocab = 4999)
+ndarrays = vectors_to_list(vectors)
+yalda_model.add_documents(ndarrays)
+yalda_model.resample(niter=100)
+### compare \lambdas, etc .. (?)
 
+yalda_counts = yalda_model.topic_word_counts
+# I believe the topic_word_counts is analogous to 
+# the \lambda in gensim.
+
+'''
+In [207]: y_counts[1][0]/float(sum(y_counts[1]))
+Out[207]: 0.98250900075680858
+'''
 
 def vectors_to_list(feature_vectors):
     '''
